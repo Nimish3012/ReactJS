b60bd33232fe57a62e0ddba65aaa436a7414643d
Nimish3012 <nimishsahu822@gmail.com>
2024-07-04 11:10:59 +0530

    Till CH 6

--- CH5/src/components/Body.js
+++ CH5/src/components/Body.js
@@ -1,63 +1,123 @@
 import { useState } from "react";
 import { resList } from "./Constants";
-import RestaurantCard  from "./RestaurantCard";
+import RestaurantCard from "./RestaurantCard";
 
-function filterData(searchText , restaurants )
-{
-  if(searchText==="")
-  {
+function filterRestaurants(searchText, restaurants) {
+  if (searchText === "") {
     return resList;
   }
-  const filterData =  restaurants.filter((restaurant)=> 
-    restaurant.info.name.includes(searchText)
-);
-if(filterData=="")
-    {
-      window.alert("Not Found");
-      return resList;
-    }
-return filterData;
+
+  const filteredData = restaurants.filter((restaurant) =>
+  (restaurant.info.name.includes(searchText) 
+  || restaurant.info.cuisines.includes(searchText)
+  || restaurant.info.avgRating==(searchText)
+    ) 
+  );
+
+  if (filteredData.length === 0) {
+    window.alert("Not Found");
+    return resList;
+  }
+
+  return filteredData;
 }
+
 const Body = () => {
-  
-  const [restaurants , setRestaurants] = useState(resList);
-  const [searchText , setSearchText] = useState();
-    return (
-      <div className="body">
+  const [restaurants, setRestaurants] = useState(resList);
+  const [searchText, setSearchText] = useState("");
 
-        <div className=" search-container">
-          {
-            <input 
-              type="rext"
-              className="search-input"
-              placeholder="Search"
-              value={searchText}
-              onChange={(e) => {
-                setSearchText(e.target.value);
-              }}
-
-              /> 
-          }
-          <button
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button
           className="search-btn"
           onClick={() => {
-            const data = filterData(searchText , restaurants);
+            const data = filterRestaurants(searchText, restaurants);
             setRestaurants(data);
           }}
-          > 
-            search
-          </button>
+        >
+          Search
+        </button>
+      </div>
+      <div className="res-container">
+        {restaurants.map((res) => (
+          <RestaurantCard key={res.info.id} resData={res} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
+
+
+// import { useState } from "react";
+// import { resList } from "./Constants";
+// import RestaurantCard  from "./RestaurantCard";
+
+// function filterData(searchText , restaurants )
+// {
+//   if(searchText==="")
+//   {
+//     return resList;
+//   }
+//   const filterData =  restaurants.filter((restaurant)=> 
+//     restaurant.info.name.includes(searchText)
+// );
+// if(filterData=="")
+//     {
+//       window.alert("Not Found");
+//       return resList;
+//     }
+// return filterData;
+// }
+// const Body = () => {
+  
+//   const [restaurants , setRestaurants] = useState(resList);
+//   const [searchText , setSearchText] = useState();
+//     return (
+//       <div className="body">
+
+//         <div className=" search-container">
+//           {
+//             <input 
+//               type="rext"
+//               className="search-input"
+//               placeholder="Search"
+//               value={searchText}
+//               onChange={(e) => {
+//                 setSearchText(e.target.value);
+//               }}
+
+//               /> 
+//           }
+//           <button
+//           className="search-btn"
+//           onClick={() => {
+//             const data = filterData(searchText , restaurants);
+//             setRestaurants(data);
+//           }}
+//           > 
+//             search
+//           </button>
           
 
-        </div>
+//         </div>
         
-        <div className="res-container">
-          {restaurants.map((res) => (
-            <RestaurantCard key={res.info.id} resData={res} />
-          ))}
-        </div>
-      </div>
-    );
-  };
+//         <div className="res-container">
+//           {restaurants.map((res) => (
+//             <RestaurantCard key={res.info.id} resData={res} />
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   };
 
-  export default Body;
+//   export default Body;

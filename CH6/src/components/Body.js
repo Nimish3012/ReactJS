bfbef7dc12fb8bb6b0d52240410771a582ce4f21
Nimish3012 <nimishsahu822@gmail.com>
2024-07-07 14:23:22 +0530

    CH 6

--- CH6/src/components/Body.js
+++ CH6/src/components/Body.js
@@ -1,31 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { resList } from "./Constants";
 import RestaurantCard from "./RestaurantCard";
-
 function filterRestaurants(searchText, restaurants) {
   if (searchText === "") {
-    return resList;
+    return restaurants;
   }
 
   const filteredData = restaurants.filter((restaurant) =>
-  (restaurant.info.name.includes(searchText) 
-  || restaurant.info.cuisines.includes(searchText)
+  
+    (restaurant?.info?.name?.toLowerCase()?.includes(searchText.toLowerCase()) 
+  || restaurant?.info?.cuisines?.map(cuisine => cuisine.toLowerCase()).join(', ').includes(searchText.toLowerCase())
   || restaurant.info.avgRating==(searchText)
     ) 
   );
 
+ 
+
   if (filteredData.length === 0) {
     window.alert("Not Found");
-    return resList;
+    return restaurants;
   }
 
   return filteredData;
 }
 
+    //dapi  data  cards  4  card card  gridElements infoWithStyle restaurants 
 const Body = () => {
   const [restaurants, setRestaurants] = useState(resList);
+  const [filteredRes, setFilteredRes] = useState(resList);
+
   const [searchText, setSearchText] = useState("");
 
+  useEffect(()=> {
+    //API CALL
+    getRestro();
+  },[])
+
+  async function getRestro() {
+    try {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      if(!data.ok){
+        throw new Error('error fetching url'); 
+      }
+      const json = await data.json();
+      setRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants , );
+      setFilteredRes(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants , );
+    } catch (error) {
+      console.error('error while fetching data', error);
+      
+    }
+  }
+
+
+  
   return (
     <div className="body">
       <div className="search-container">
@@ -40,14 +67,14 @@ const Body = () => {
           className="search-btn"
           onClick={() => {
             const data = filterRestaurants(searchText, restaurants);
-            setRestaurants(data);
+            setFilteredRes(data);
           }}
         >
           Search
         </button>
       </div>
       <div className="res-container">
-        {restaurants.map((res) => (
+        {filteredRes.map((res) => (
           <RestaurantCard key={res.info.id} resData={res} />
         ))}
       </div>
